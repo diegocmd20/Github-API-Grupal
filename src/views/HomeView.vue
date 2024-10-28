17db93e57c38b88f463eb4f663a4a55cd72d62e8
diegocmd20 Home <diegocmd20@gmail.com>
2024-10-28 20:52:59 -0300

    Project began

---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<script>
+  export default {
+    name: "HomeView",
+    data(){
+      return {
+        url: "https://avatars.githubusercontent.com/u/583231?v=4",
+      }
+    }
+  }
+</script>
+
+<template>
+  <main>
+    <div class="container">
+      <h1 class="display-3">Bienvenidos a la Galería Github</h1>
+      <img :src="url" class="h-25 round rounded-circle" alt="Octocat">
+    </div>
+  </main>
+</template>
